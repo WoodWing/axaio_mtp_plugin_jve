54b112cea5a6462bfffe3b1096d3d808e333aa8c
autobuild <ekl@woodwing.com>
2016-06-24 11:24:57 +0200

    [Ent Server 10.1.0] Jenkins: Updated product version info files with 43.
    Former-commit-id: 3226440d8d79338847dce8f2673d547e9a752882

--- Enterprise/WordPress/plugins/ww_enterprise/ww_enterprise.php
+++ Enterprise/WordPress/plugins/ww_enterprise/ww_enterprise.php
@@ -3,7 +3,7 @@
  * Plugin Name: WoodWing Enterprise
  * Plugin URI: http://woodwing.com
  * Description: This plugin extends some functionality from WordPress and is used by Enterprise Server.
- * Version: 10.0.0 Build 926
+ * Version: 10.1.0 Build 43
  * Author: WoodWing Software
  */
 
