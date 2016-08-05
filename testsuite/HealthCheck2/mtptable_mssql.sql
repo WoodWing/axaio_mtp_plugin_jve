560fc74d70706ce857201ad0da4026878c2a6875
Jeroen Venderbosch <jve@woodwing.com>
2016-08-05 15:43:23 +0200

    Update mtptable_mssql.sql
    The 'unsigned' keyword is incorrect for Mssql.

--- testsuite/HealthCheck2/mtptable_mssql.sql
+++ testsuite/HealthCheck2/mtptable_mssql.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE [axaio_mtp_trigger] (
   [publication_id] int not null ,
   [issue_id] int not null  default '0',
@@ -9,18 +8,18 @@ CREATE TABLE [axaio_mtp_trigger] (
   [state_after_article] int not null  default 0,
   [state_after_image] int not null  default 0,
   [mtp_jobname] text not null  default '',
-  [state_error_layout] int(11) NOT NULL DEFAULT '0',
-  [quiet] tinyint(1) unsigned DEFAULT '0',
-  [prio] tinyint(1) unsigned DEFAULT '2',
+  [state_error_layout] int(11) NOT NULL DEFAULT 0,
+  [quiet] tinyint DEFAULT 0,
+  [prio] tinyint DEFAULT 2,
 
   PRIMARY KEY ([publication_id], [issue_id], [state_trigger_layout],[state_trigger_article],[state_trigger_image])
 );
 CREATE  INDEX [ii_mtp] on [axaio_mtp_trigger]([issue_id]) ;
 
 CREATE TABLE [axaio_mtp_sentobjects] (
-  [objid] int not null  default '0',
+  [objid] int not null  default 0,
   [publication_id] int not null ,
-  [issue_id] int not null  default '0',
+  [issue_id] int not null  default 0,
   [state_trigger_layout] int not null ,
   [printstate] int not null ,
   PRIMARY KEY ([objid], [publication_id], [issue_id], [state_trigger_layout], [printstate])
