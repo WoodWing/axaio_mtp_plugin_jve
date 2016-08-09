f289c7f545a635517fe33c1bbc7ad9fc3076f9c3
Jeroen Venderbosch <jve@woodwing.com>
2016-08-09 16:05:58 +0200

    Make the plug-in Mssql compatible.

--- testsuite/HealthCheck2/AxaioMadeToPrint_TestCase.php
+++ testsuite/HealthCheck2/AxaioMadeToPrint_TestCase.php
@@ -6,7 +6,7 @@
  *
  * @package Enterprise
  * @subpackage TestSuite
- * @since v9.9.0
+ * @since v9.8.2
  * @copyright WoodWing Software bv. All Rights Reserved.
  */
 
@@ -20,7 +20,7 @@ class WW_TestSuite_HealthCheck2_AxaioMadeToPrint_TestCase extends TestCase
     public function getTestGoals()   { return 'Checks if axaio MadeToPrint is installed and configured correctly.'; }
     public function getTestMethods() { return 'Check for required Tables and Triggers'; }
     public function getPrio()        { return 0; }
-
+	
 	final public function runTest()
 	{
         LogHandler::Log('wwtest', 'INFO', 'Testing MadeToPrint configuration');
@@ -28,7 +28,7 @@ class WW_TestSuite_HealthCheck2_AxaioMadeToPrint_TestCase extends TestCase
         if( !$this->checkInstalled() ) {
 			return;
 		}
-
+        
         // check server version
         // SERVERVERSION => '9.2.0 Build 53'
         $version = array();
@@ -41,18 +41,18 @@ class WW_TestSuite_HealthCheck2_AxaioMadeToPrint_TestCase extends TestCase
 			$this->setResult( 'ERROR', "AxaioMadeToPrint requires Enterprise 9.2.0 or newer", "" );
 			return;
 		}
-
+        
         // check for old settings, mostly from earlier integrations
         $this->checkObsoletedSettings();
-
+    
         // check folder if they are writable
-        if( !$this->checkMTPFolder( 'AXAIO_MTP_SERVER_FOLDER_IN')
-		||	!$this->checkMTPFolder( 'AXAIO_MTP_AXAIO_FOLDER_IN' )
-		||	!$this->checkMTPFolder( 'AXAIO_MTP_AXAIO_FOLDER_OUT')
+        if( !$this->checkMTPFolder( 'AXAIO_MTP_SERVER_FOLDER_IN') 
+		||	!$this->checkMTPFolder( 'AXAIO_MTP_AXAIO_FOLDER_IN' ) 
+		||	!$this->checkMTPFolder( 'AXAIO_MTP_AXAIO_FOLDER_OUT') 
         ) {
 			return;
 		}
-
+        
         /*
         if( !$this->checkURLResponse( 'AXAIO_MTP_PREPROCESS_LOC' )
         ||  !$this->checkURLResponse( 'AXAIO_MTP_POSTPROCESS_LOC')
@@ -60,7 +60,7 @@ class WW_TestSuite_HealthCheck2_AxaioMadeToPrint_TestCase extends TestCase
             return;
         }
         */
-		// check if the MtP user is configured and try to logon/logoff
+		// check if the MtP user is configured and try to logon/logoff 
 		if( trim(AXAIO_MTP_USER) == '' ) {
 			$help = 'Please check AXAIO_MTP_USER option at config/plugins/AxaioMadeToPrint/config.php.';
 			$this->setResult( 'ERROR', "No MadeToPrint user name specified.", $help );
@@ -91,24 +91,18 @@ class WW_TestSuite_HealthCheck2_AxaioMadeToPrint_TestCase extends TestCase
             $this->setResult( 'ERROR', 'Failed to logoff the configured MadeToPrint user. Error returned from server: '.$e->getMessage(), 'Please check AXAIO_MTP_USER and AXAIO_MTP_PASSWORD options at configserver.php.');
 				return;
 		}
-
+		
 
 		$sql_error = '';
 		$dbDriver = DBDriverFactory::gen();
 
 		$check_tables = array('axaio_mtp_trigger', 'axaio_mtp_sentobjects');
 		$not_found_tables =array();
-		foreach($check_tables as $value)
-		{
-			$sql = "SHOW TABLES LIKE '$value'";
-			$sth = $dbDriver->query($sql);
-			$return = $dbDriver->fetch($sth);
-			if( $return == false )
-			{
-				$not_found_tables[] = $value;
-			}
-
-		}
+        foreach ($check_tables as $value) {
+            if (!$dbDriver->tableExists($value, false)) {
+                $not_found_tables[] = $value;
+            }
+        }
 		if(!empty($not_found_tables))
 		{
 			$tables = (count($not_found_tables) == 1) ? 'Table' : 'Tables';
@@ -128,20 +122,20 @@ class WW_TestSuite_HealthCheck2_AxaioMadeToPrint_TestCase extends TestCase
 	 *
 	 * @return boolean TRUE when 'good enough'. FALSE when is makes no sense to continue testing.
 	 */
-	private function checkMTPFolder( $constName )
+	private function checkMTPFolder( $constName ) 
     {
         $folder = constant($constName);
         if( empty($folder)) { // NULL or '' or not defined properly
             $this->setResult( 'ERROR', "{$constName} is not specified.", "Please define and set the {$constName} option at the plugin config.php.");
             return false;
         }
-
+        
         if( substr($folder, -1, 1) != '/' && substr($folder, -1, 1) != '\\' ) {
             $this->setResult( 'ERROR', "The specified MadeToPrint folder {$constName} has no slash (/) or backslash (\\) at the end.", "Please check {$constName} option at configserver.php.");
             return false;
         }
 
-
+        
         $tokenBCnt = (defined('AXAIO_MTP_TOKEN_BEGIN'))?substr_count($folder, AXAIO_MTP_TOKEN_BEGIN):0;
         $tokenECnt = (defined('AXAIO_MTP_TOKEN_END')  )?substr_count($folder, AXAIO_MTP_TOKEN_END  ):0;
         if($tokenBCnt && $tokenBCnt!=$tokenECnt) {
@@ -200,7 +194,7 @@ class WW_TestSuite_HealthCheck2_AxaioMadeToPrint_TestCase extends TestCase
 				return FALSE;
 			}
 		}
-
+		
 		// Check obsoleted defines
 		$defines = array( 'AXAIO_MTP_INFOLDER_PER_PUBLICATION','AXAIO_MTP_ALL_METADATA' );
 		foreach( $defines as $define ) {
@@ -209,7 +203,7 @@ class WW_TestSuite_HealthCheck2_AxaioMadeToPrint_TestCase extends TestCase
 				// continue
 			}
 		}
-
+		
 		return TRUE;
 	}
     /**
@@ -231,7 +225,7 @@ class WW_TestSuite_HealthCheck2_AxaioMadeToPrint_TestCase extends TestCase
             $this->setResult( 'ERROR', "The specified {$constName} seems to be malformed.", "Please check {$constName} option at configserver.php.");
             return FALSE;
         }
-
+	
         $host = $urlParts["host"];
         $port = isset($urlParts["port"]) ? $urlParts["port"] : 80;
         $errno = 0;
@@ -246,25 +240,25 @@ class WW_TestSuite_HealthCheck2_AxaioMadeToPrint_TestCase extends TestCase
 
 
 	/**
-	 * This method checks if the mandatory fields are set in config_solr.php  and if
+	 * This method checks if the mandatory fields are set in config_solr.php  and if 
 	 * they match with the settings of schema.xml
 	 *
 	 * @param array $schemaInfo Info read from the schema.xml file.
 	 */
-	private function checkMandatoryFields($schemaInfo)
+	private function checkMandatoryFields($schemaInfo) 
 	{
 		$mandatoryFields = array('ID', 'PublicationId', 'IssueId', 'CategoryId', 'StateId', 'IssueIds', 'Issues', 'Closed');
-
+		
 		foreach ($mandatoryFields as $mandatoryField) {
 			if (!array_key_exists($mandatoryField, $schemaInfo)) {
 					$this->setResult( 'ERROR', 'Mandatory field ' . "'$mandatoryField'" . ' not in &lt;fields&gt; section of schema.xml.' , 'Check &lt;fields&gt; tag in schema.xml.');
-			}
+			}		
 			elseif ($schemaInfo[$mandatoryField]['indexed'] !== 'true') {
 					$this->setResult( 'ERROR', "Mandatory field $mandatoryField must be indexed." , "Add 'indexed = \"true\"' on field $mandatoryField in schema.xml.");
 			}
-		}
+		} 
 	}
-
+    
     /**
 	 * Checks all MadeToPrint options at the configserver.php file.
 	 * This check is not done at wwtest since the MadeToPrint integrations is optional.
@@ -278,8 +272,8 @@ class WW_TestSuite_HealthCheck2_AxaioMadeToPrint_TestCase extends TestCase
 		if( trim(AXAIO_MTP_SERVER_DEF_ID) == '' ) {
 			return 'MadeToPrint is disabled. Set options at configserver.php to enable. The AXAIO_MTP_SERVER_DEF_ID option tells if MadeToPrint is enabled or not.';
 		}
-
-		// Check if in/out folders are configured correctly and accessable
+	
+		// Check if in/out folders are configured correctly and accessable	
 		if( trim(AXAIO_MTP_SERVER_FOLDER_IN) == '' ) {
 			return 'No MadeToPrint in-folder specified. Please check AXAIO_MTP_SERVER_FOLDER_IN option at configserver.php.';
 		}
@@ -292,21 +286,21 @@ class WW_TestSuite_HealthCheck2_AxaioMadeToPrint_TestCase extends TestCase
 		if(!is_writable(AXAIO_MTP_SERVER_FOLDER_IN)){
 			return 'No write access to specified MadeToPrint in-folder. Please check file system and AXAIO_MTP_SERVER_FOLDER_IN option at configserver.php.';
 		}
-
+		
 		if( trim(AXAIO_MTP_AXAIO_FOLDER_IN) == '' ) {
 			return 'No MadeToPrint in-folder specified. Please check AXAIO_MTP_AXAIO_FOLDER_IN option at configserver.php.';
 		}
 		if( strrpos(AXAIO_MTP_AXAIO_FOLDER_IN,'/') != (strlen(AXAIO_MTP_AXAIO_FOLDER_IN)-1) ) {
 			return 'The specified MadeToPrint in-folder has no slash (/) at the end. Please check AXAIO_MTP_AXAIO_FOLDER_IN option at configserver.php.';
 		}
-
+		
 		if( trim(AXAIO_MTP_AXAIO_FOLDER_OUT) == '' ) {
 			return 'No MadeToPrint out-folder specified. Please check AXAIO_MTP_AXAIO_FOLDER_OUT option at configserver.php.';
 		}
 		if( strrpos(AXAIO_MTP_AXAIO_FOLDER_OUT,'/') != (strlen(AXAIO_MTP_AXAIO_FOLDER_OUT)-1) ) {
 			return 'The specified MadeToPrint out-folder has no slash (/) at the end. Please check AXAIO_MTP_AXAIO_FOLDER_IN option at configserver.php.';
 		}
-
+	
 		// Check if post process is configured and can be ping-ed
 		if( trim(AXAIO_MTP_POSTPROCESS_LOC) == '' ) {
 			return 'No MadeToPrint post process specified. Please check AXAIO_MTP_POSTPROCESS_LOC option at configserver.php.';
@@ -324,8 +318,8 @@ class WW_TestSuite_HealthCheck2_AxaioMadeToPrint_TestCase extends TestCase
 			return 'The specified MadeToPrint post process is not responsive ('.$errstr.'). Please check AXAIO_MTP_POSTPROCESS_LOC option at configserver.php.';
 		}
 		fclose( $socket );
-
-		// Check if the MtP user is configured and try to logon/logoff
+	
+		// Check if the MtP user is configured and try to logon/logoff 
 		if( trim(AXAIO_MTP_USER) == '' ) {
 			return 'No MadeToPrint user name specified. Please check AXAIO_MTP_USER option at configserver.php.';
 		}
@@ -351,7 +345,7 @@ class WW_TestSuite_HealthCheck2_AxaioMadeToPrint_TestCase extends TestCase
 			return 'Failed to logoff the configured MadeToPrint user. Please check AXAIO_MTP_USER and AXAIO_MTP_PASSWORD options at configserver.php.'.
 					'Error returned from server: '.$e->getMessage();
 		}
-
+		
 		return '';
 	}
 
