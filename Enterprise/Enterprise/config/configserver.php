8356fbe3d24121c741ed4c4c80f5f9f80dfc15d9
Edwin van der Klaauw <edwin.van.der.klaauw@EklBookPro.local>
2016-08-16 09:50:30 +0200

    [Ent Server 10.1.0] Correct a merge failure for the MESSAGE_QUEUE_CONNECTIONS option causing PHP notices. As a result the Health Check and Build Test failed with "String could not be parsed as XML".
    Former-commit-id: b4cbde5aba08914cdbcc6210ca525479ba000df9

--- Enterprise/Enterprise/config/configserver.php
+++ Enterprise/Enterprise/config/configserver.php
@@ -1389,16 +1389,18 @@ if( !defined('GHOST_SCRIPT_APP_PATH') ) {
 //    1. Uncomment the required MessageQueueConnection definitions below by removing the leading slashes (//).
 //    2. For those definitions, replace 'localhost' with the hostname (or IP) of the system on which RabbitMQ is installed.
 //
-define( 'MESSAGE_QUEUE_CONNECTIONS', serialize(array(
-// - - - - Unsecure connection over TCP: - - - -
-//  new MessageQueueConnection( 'RabbitMQ', 'AMQP', 'amqp://localhost:5672', true, 'woodwing', 'ww' ),
-//  new MessageQueueConnection( 'RabbitMQ', 'REST', 'http://localhost:15672', true, 'woodwing', 'ww' ),
-//  new MessageQueueConnection( 'RabbitMQ', 'STOMPWS', 'ws://localhost:15674/ws', true, 'woodwing', 'ww' ),
-// - - - - Secure connection over SSL: - - - -
-//  new MessageQueueConnection( 'RabbitMQ', 'AMQP', 'amqps://localhost:5671', true, 'woodwing', 'ww' ),
-//  new MessageQueueConnection( 'RabbitMQ', 'REST', 'https://localhost:15671', true, 'woodwing', 'ww' ),
-//  new MessageQueueConnection( 'RabbitMQ', 'STOMPWS', 'wss://localhost:15673/ws', true, 'woodwing', 'ww' ),
-)));
+if( !defined('MESSAGE_QUEUE_CONNECTIONS') ) {
+	define( 'MESSAGE_QUEUE_CONNECTIONS', serialize(array(
+	// - - - - Unsecure connection over TCP: - - - -
+	//  new MessageQueueConnection( 'RabbitMQ', 'AMQP', 'amqp://localhost:5672', true, 'woodwing', 'ww' ),
+	//  new MessageQueueConnection( 'RabbitMQ', 'REST', 'http://localhost:15672', true, 'woodwing', 'ww' ),
+	//  new MessageQueueConnection( 'RabbitMQ', 'STOMPWS', 'ws://localhost:15674/ws', true, 'woodwing', 'ww' ),
+	// - - - - Secure connection over SSL: - - - -
+	//  new MessageQueueConnection( 'RabbitMQ', 'AMQP', 'amqps://localhost:5671', true, 'woodwing', 'ww' ),
+	//  new MessageQueueConnection( 'RabbitMQ', 'REST', 'https://localhost:15671', true, 'woodwing', 'ww' ),
+	//  new MessageQueueConnection( 'RabbitMQ', 'STOMPWS', 'wss://localhost:15673/ws', true, 'woodwing', 'ww' ),
+	)));
+}
 
 // -------------------------------------------------------------------------------------------------
 // Debugging - Low-level logging of SQL, SOAP details etc.
