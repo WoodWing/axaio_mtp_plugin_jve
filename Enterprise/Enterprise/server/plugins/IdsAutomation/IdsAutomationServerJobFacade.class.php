98e279650f26c73b0ac6df43d2c21701e7262383
Ruud Jungbacker <rju@woodwing.com>
2016-09-01 15:29:33 +0200

    [Ent Server 10.1.0][WR-548] Some rework on commit b67828a.
    Former-commit-id: fd33a701fe461afb2b6f95a7effab7ff3c304a17

--- Enterprise/Enterprise/server/plugins/IdsAutomation/IdsAutomationServerJobFacade.class.php
+++ Enterprise/Enterprise/server/plugins/IdsAutomation/IdsAutomationServerJobFacade.class.php
@@ -4,6 +4,9 @@
  * @subpackage  ServerPlugins
  * @since       v10.0.2
  * @copyright   WoodWing Software bv. All Rights Reserved.
+ *
+ * This facade gives other server plug-ins easy access to create IdsAutomation jobs. 
+ * It is important to keep the interface backwards compatible!
  */
 
 class IdsAutomationServerJobFacade
@@ -20,20 +23,22 @@ class IdsAutomationServerJobFacade
 	 * @param integer|null $stateId
 	 * @return boolean|null Returns true if a job was created, otherwise false. Null is returned when the plugin isn't activated
 	 */
-	public static function createIdsAutomationJobsForLayout( $objId, $objType = null, $stateId = null ) {
+	public static function createIdsAutomationJobsForLayout( $objId, $objType = null, $stateId = null )
+	{
 		require_once dirname(__FILE__) . '/IdsAutomationUtils.class.php';
-		if ( IdsAutomationUtils::isPluginActivated() ) {
-			if (!$objType) {
-				$objType = IdsAutomationUtils::getObjectType($objId);
-			}
-			if (!$stateId) {
-				$stateId = IdsAutomationUtils::getStatusId($objId);
-			}
-
-			return IdsAutomationUtils::createIdsAutomationJobsForLayout($objId, $objType, $stateId);
-		} else {
+		if ( !IdsAutomationUtils::isPluginActivated() ) {
 			LogHandler::Log('IdsAutomation', 'DEBUG', 'Skip creating IdsAutomation jobs since the plugin is deactivated.');
+			return null;
 		}
+
+		if (!$objType) {
+			$objType = IdsAutomationUtils::getObjectType($objId);
+		}
+		if (!$stateId) {
+			$stateId = IdsAutomationUtils::getStatusId($objId);
+		}
+
+		return IdsAutomationUtils::createIdsAutomationJobsForLayout($objId, $objType, $stateId);
 	}
 
 	/**
@@ -50,19 +55,21 @@ class IdsAutomationServerJobFacade
 	 * @param integer|null $stateId
 	 * @return boolean|null Returns true if a job was created, otherwise false. Null is returned when the plugin isn't activated
 	 */
-	public static function createIdsAutomationJobsForPlacedObject( $objId, $objType = null, $stateId = null ) {
+	public static function createIdsAutomationJobsForPlacedObject( $objId, $objType = null, $stateId = null )
+	{
 		require_once dirname(__FILE__) . '/IdsAutomationUtils.class.php';
-		if ( IdsAutomationUtils::isPluginActivated() ) {
-			if ( !$objType ) {
-				$objType = IdsAutomationUtils::getObjectType($objId);
-			}
-			if ( !$stateId ) {
-				$stateId = IdsAutomationUtils::getStatusId($objId);
-			}
-
-			return IdsAutomationUtils::createIdsAutomationJobsForPlacedObject($objId, $stateId, $objType);
-		} else {
+		if ( !IdsAutomationUtils::isPluginActivated() ) {
 			LogHandler::Log('IdsAutomation', 'DEBUG', 'Skip creating IdsAutomation jobs since the plugin is deactivated.');
+			return null;
 		}
+
+		if ( !$objType ) {
+			$objType = IdsAutomationUtils::getObjectType($objId);
+		}
+		if ( !$stateId ) {
+			$stateId = IdsAutomationUtils::getStatusId($objId);
+		}
+
+		return IdsAutomationUtils::createIdsAutomationJobsForPlacedObject($objId, $stateId, $objType);
 	}
 }
