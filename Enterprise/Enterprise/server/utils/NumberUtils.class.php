7109403bc4d0b0362760bdbea466fb3909c767d3
Jeroen Venderbosch <jve@woodwing.com>
2016-12-20 13:37:00 +0100

    [Ent Server 10.0.4] EN-88429 Empty page order must be ignored when page number, - range is calculated.
    Former-commit-id: d0816c34d8865504c9bfc7c9457d7c0eb51b5545

--- Enterprise/Enterprise/server/utils/NumberUtils.class.php
+++ Enterprise/Enterprise/server/utils/NumberUtils.class.php
@@ -35,24 +35,22 @@ class NumberUtils
 	 * 
 	 * The input is sorted and cleaned from duplicates
 	 * 
-	 * @param array of numbers $pagenumbers
+	 * @param array of numbers $pageNumbers
 	 * @param integer $maxLen
 	 * @return string
 	 */
-	public static function createNumberRange( $pagenumbers, $maxLen=40 )
+	public static function createNumberRange( $pageNumbers, $maxLen=40 )
 	{
 		$pagerange = '';
-		// Remove duplicate page numbers and sort
-		$pagenumbers = array_unique( $pagenumbers );
-		sort( $pagenumbers );
-		$pageNumberCount = count( $pagenumbers );
+		$pageNumbers = self::sanitizePageNumbersForRangeCalc( $pageNumbers );
+		$pageNumberCount = count( $pageNumbers );
 		$lastPageNumber="";
 		if( $pageNumberCount >= 1 ) {
-			$pagerange = $pagenumbers[0];
+			$pagerange = $pageNumbers[0];
 			for( $pgn=1 ; $pgn < $pageNumberCount; ++$pgn ) {
-				if( $pagenumbers[$pgn] == $pagenumbers[$pgn-1]+1 ) {
+				if( $pageNumbers[$pgn] == $pageNumbers[$pgn-1]+1 ) {
 					// consecutive page number, remember last number, don't add to string yet
-					$lastPageNumber = $pagenumbers[$pgn];
+					$lastPageNumber = $pageNumbers[$pgn];
 				}
 				else {
 					// non-consecutive, see if we need to close previous range and add this number:
@@ -60,7 +58,7 @@ class NumberUtils
 						$pagerange .= "-".$lastPageNumber;
 						$lastPageNumber = "";
 					}
-					$pagerange .= ",".$pagenumbers[$pgn];
+					$pagerange .= ",".$pageNumbers[$pgn];
 				}
 			}
 			// See if we have a open range we need to close:
@@ -72,9 +70,27 @@ class NumberUtils
 		
 		// Too long, change to <start>~<end>
 		if( strlen($pagerange) > $maxLen ) {
-			$pagerange = $pagenumbers[0] . ' ~ ' . $pagenumbers[count($pagenumbers)-1];
+			$pagerange = $pageNumbers[0] . ' ~ ' . $pageNumbers[count($pageNumbers)-1];
 		}
 		return $pagerange;
+	}
+
+	/**
+	 * Sanitizes the page numbers before the page ranges are calculated.
+	 *
+	 * Empty page numbers are removed. E.g. in case of a placement on the pasteboard the page number is null or ''.
+	 * Duplicates are removed and the numbers are sorted.
+	 *
+	 * @param array $pageNumbers
+	 * @return array sanitized page numbers.
+	 */
+	static private function sanitizePageNumbersForRangeCalc( array $pageNumbers )
+	{
+		$sanitizedPageNumbers = array_filter( $pageNumbers, function( $pageNumber) { return !empty( $pageNumber ); } );
+		$sanitizedPageNumbers = array_unique( $sanitizedPageNumbers );
+		sort( $sanitizedPageNumbers );
+
+		return $sanitizedPageNumbers;
 	}
 
 	/**
