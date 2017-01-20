d41a947d74200d864a4e3f358129681e646f203b
Edwin van der Klaauw <edwin.van.der.klaauw@EklBookPro.local>
2017-01-20 18:35:18 +0800

    [Ent Server 10.2.0] [EN-88597] ZF 2.4.9 shipped with ES 10.0/10.1  is not compatible with Elvis Cloud API
    Former-commit-id: 887cac869126bf3339fb8e254a0a1cc2d5845431

--- Enterprise/Enterprise/server/vendor/composer/autoload_namespaces.php
+++ Enterprise/Enterprise/server/vendor/composer/autoload_namespaces.php
@@ -8,6 +8,7 @@ $baseDir = dirname($vendorDir);
 return array(
     'ZendXml\\' => array($vendorDir . '/zendframework/zendxml/library'),
     'Solarium\\' => array($vendorDir . '/solarium/solarium/library'),
+    'Prophecy\\' => array($vendorDir . '/phpspec/prophecy/src'),
     'Guzzle\\Tests' => array($vendorDir . '/guzzle/guzzle/tests'),
     'Guzzle' => array($vendorDir . '/guzzle/guzzle/src'),
     'Graze\\Guzzle\\JsonRpc' => array($vendorDir . '/graze/guzzle-jsonrpc/lib'),
