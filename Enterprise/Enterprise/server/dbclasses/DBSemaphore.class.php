d19dd47185fe165e245892af73715eeca8eb56e1
Jeroen Venderbosch <jve@woodwing.com>
2017-05-02 13:17:29 +0200

    [Ent Server 10.1.3] EN-88960 Rework on previous changes, use the id of the semaphore to release it, suppress the logging of sql statements to create and release the semaphores, add the type of the function parameters.
    Former-commit-id: 53829c6da99c4bb3574a6733560f8a7e95052957

--- Enterprise/Enterprise/server/dbclasses/DBSemaphore.class.php
+++ Enterprise/Enterprise/server/dbclasses/DBSemaphore.class.php
@@ -21,10 +21,11 @@ class DBSemaphore extends DBBase
 	 * @param string $entityId The id of any entity. For example, the issue id for which a publishing operation runs.
 	 * @param integer $lifeTime Seconds to keep up the semaphore in case process ends unexpectedly.
 	 * @param string $userShort User for whom the process is created that needs the semaphore.
+	 * @param bool $logSql Whether or not the SQL will be logged.
 	 * @throws BizException on DB error.
 	 * @return integer Semaphore id.
 	 */
-	static public function createSemaphore( $entityId, $lifeTime, $userShort )
+	static public function createSemaphore( $entityId, $lifeTime, $userShort, $logSql  )
 	{
 		// Init.
 		$dbDriver = DBDriverFactory::gen();
@@ -37,7 +38,7 @@ class DBSemaphore extends DBBase
 		$where = "`entityid` = ? AND `lastupdate` < ? ";
 		$params = array( $entityId, $expirationTime );
 	    try {
-			self::deleteRows( self::TABLENAME, $where, $params );
+			self::deleteRows( self::TABLENAME, $where, $params, $logSql );
 		} catch ( BizException $e ) {
 			LogHandler::Log( __CLASS__ ,'WARN', 'Cleaning up old semaphores failed: '.$e->getMessage().' Just continue.' );
 		}		
@@ -45,7 +46,7 @@ class DBSemaphore extends DBBase
 		$params = array( $entityId, $userShort, $ipAddress, $now, $lifeTime );
 		$sql = "INSERT INTO $dbTable (`entityid`, `user`, `ip`, `lastupdate`, `lifetime`) VALUES (?, ?, ?, ?, ?)";
 		$sql = $dbDriver->autoincrement( $sql );
-		$sth = $dbDriver->query( $sql, $params, null, true, false );
+		$sth = $dbDriver->query( $sql, $params, null, $logSql, false );
 
 		// Suppress 'already' exists error.
 		if( !$sth ) {
@@ -65,13 +66,14 @@ class DBSemaphore extends DBBase
 	 * Release the semaphore obtained through createSemaphore().
 	 *
 	 * @param integer $semaId Semaphore (id)
+	 * @param bool $logSQL Whether or not the resulting SQL must be logged.
 	 * @return boolean
 	 */
-	static public function releaseSemaphore( $semaId )
+	static public function releaseSemaphore( $semaId, $logSQL )
 	{
 		$where = "`id` = ?";
 		$params = array( $semaId );
-		return (bool)self::deleteRows( self::TABLENAME, $where, $params );
+		return (bool)self::deleteRows( self::TABLENAME, $where, $params, $logSQL );
 	}
 	
 	/**
