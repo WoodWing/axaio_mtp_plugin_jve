43429078cb2920b980eb78ea2cdab8eea0c4dea8
Edwin van der Klaauw <edwin.van.der.klaauw@EklBookPro.local>
2017-05-30 12:52:25 +0200

    [Ent Server 10.2.0 [EP-2462] Rework on build script: Quote paths that contain spaces to avoid build error.
    09:42:30 + echo 'step5c: Determine the last Enterprise Server version that introduced a new ionCube Encoder.'
    09:42:30 step5c: Determine the last Enterprise Server version that introduced a new ionCube Encoder.
    09:42:30 ++ php56 '"/home/autobuild/workspace/Enterprise' Server Build 'Tools/php_define.php"' --get --define=WW_ES_BASE_VERSION_FOR_IONCUBE --stripquotes
    09:42:30 + esBaseVersion='Could not open input file: "/home/autobuild/workspace/Enterprise'
    09:42:31 Build step 'Execute shell' marked build as failure
    
    Former-commit-id: 74545e29e3fde593ec7a2d3115edbd1ce31b3f1e

--- Enterprise/Build/auto_build.sh
+++ Enterprise/Build/auto_build.sh
@@ -14,8 +14,8 @@
 SOURCE_BASE="./Enterprise/"
 TARGET_BASE="./Enterprise_release/"
 iONCUBE_ENCODER="/usr/local/ioncube/9.0/ioncube_encoder56_9.0_64"
-ES_PHP_ENCODER="\"/home/autobuild/workspace/Enterprise Server Build Tools/es_php_encoder.php\""
-ES_PHP_DEFINE="\"/home/autobuild/workspace/Enterprise Server Build Tools/php_define.php\""
+ES_PHP_ENCODER="/home/autobuild/workspace/Enterprise Server Build Tools/es_php_encoder.php"
+ES_PHP_DEFINE="/home/autobuild/workspace/Enterprise Server Build Tools/php_define.php"
 PHPSTORM_INSPECTOR="/opt/phpstorm2017.1.4/bin/inspect.sh"
 : ${PHP_EXE:=php} #Set default value to base php executable.
 
@@ -539,7 +539,7 @@ function step5_ionCubeEncodePhpFiles {
 	sync
 
 	echo 'step5c: Determine the last Enterprise Server version that introduced a new ionCube Encoder.'
-	esBaseVersion=`${PHP_EXE} ${ES_PHP_DEFINE} --get --define=WW_ES_BASE_VERSION_FOR_IONCUBE --stripquotes < "${SOURCE_BASE}/Enterprise/server/serverinfo.php"`
+	esBaseVersion=`${PHP_EXE} "${ES_PHP_DEFINE}" --get --define=WW_ES_BASE_VERSION_FOR_IONCUBE --stripquotes < "${SOURCE_BASE}/Enterprise/server/serverinfo.php"`
 	if [ ! -n "${esBaseVersion}" ]; then
 		echo "ERROR: Failed reading the WW_ES_BASE_VERSION_FOR_IONCUBE option from serverinfo.php."
 		exit 1
@@ -553,7 +553,7 @@ function step5_ionCubeEncodePhpFiles {
 	# "${iONCUBE_ENCODER}" --help | more
 
 	echo "step5e: Encode specific Enterprise core files and folders."
-	ioncubeEncode="${PHP_EXE} ${ES_PHP_ENCODER} --sourcebase=${SOURCE_BASE} --targetbase=${TARGET_BASE} --esversion=${esBaseVersion} --deployment=integrated "
+	ioncubeEncode="${PHP_EXE} "${ES_PHP_ENCODER}" --sourcebase=${SOURCE_BASE} --targetbase=${TARGET_BASE} --esversion=${esBaseVersion} --deployment=integrated "
 	icFoldersOrFiles="\
 		Enterprise/server/admin/license/ \
 		Enterprise/server/dbclasses/ \
