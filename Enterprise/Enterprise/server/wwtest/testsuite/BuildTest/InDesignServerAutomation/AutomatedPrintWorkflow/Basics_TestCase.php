b13021feac7adba7cbe9fe0bb9c371d78f6f801b
MengLu Au Yong <menglu@woodwing.com>
2017-07-18 17:53:40 +0200

    [Ent Server 10.1.4] Fixed a problem occurring in the BuildTest. It’s about the ids job in the job queue not being cleaned up, causing another build script to fail.
    Former-commit-id: e2bf72e448fea9a8cadb3a188294876dcd8f3de2

--- Enterprise/Enterprise/server/wwtest/testsuite/BuildTest/InDesignServerAutomation/AutomatedPrintWorkflow/Basics_TestCase.php
+++ Enterprise/Enterprise/server/wwtest/testsuite/BuildTest/InDesignServerAutomation/AutomatedPrintWorkflow/Basics_TestCase.php
@@ -12,7 +12,10 @@ require_once BASEDIR.'/server/wwtest/testsuite/TestSuiteInterfaces.php';
 
 class WW_TestSuite_BuildTest_InDesignServerAutomation_AutomatedPrintWorkflow_Basics_TestCase extends TestCase
 {
-	/** @var WW_Utils_TestSuite $utils */
+	/** @var string $testStartTime */
+	private $testStartTime = null;
+
+	/** @var WW_Utils_TestSuite $globalUtils */
 	private $globalUtils = null;
 
 	/** @var WW_TestSuite_BuildTest_InDesignServerAutomation_AutomatedPrintWorkflow_Utils $localUtils */
@@ -113,6 +116,8 @@ class WW_TestSuite_BuildTest_InDesignServerAutomation_AutomatedPrintWorkflow_Bas
 	 */
 	private function setupTestData()
 	{
+		$this->testStartTime = date('Y-m-d\TH:i:s');
+
 		require_once BASEDIR.'/server/utils/TestSuite.php';
 		$this->globalUtils = new WW_Utils_TestSuite();
 		
@@ -166,6 +171,8 @@ class WW_TestSuite_BuildTest_InDesignServerAutomation_AutomatedPrintWorkflow_Bas
 	 */
 	private function tearDownTestData()
 	{
+		$this->clearIdsServerJobsInTheQueue();
+
 		$objectIds = array();
 		$articleId = $this->articleObject ? $this->articleObject->MetaData->BasicMetaData->ID : null;
 		if( $articleId ) {
@@ -182,6 +189,17 @@ class WW_TestSuite_BuildTest_InDesignServerAutomation_AutomatedPrintWorkflow_Bas
 
 		$this->unlockObjects( $objectIds );
 		$this->deleteObjects( $objectIds );
+	}
+
+	/**
+	 * Clean up all the IDS server jobs in the job queue created by this build test.
+	 */
+	private function clearIdsServerJobsInTheQueue()
+	{
+		require_once BASEDIR . '/server/dbclasses/DBBase.class.php';
+		$where = '`queuetime` >= ? ';
+		$params = array( strval( $this->testStartTime ) );
+		$result = DBBase::deleteRows( 'indesignserverjobs', $where, $params );
 	}
 
 	/**
