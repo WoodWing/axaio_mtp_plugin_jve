4daea5f3b6dbd180a9c2cc1d3b07b4845ecf6298
MengLu Au Yong <menglu@woodwing.com>
2017-09-21 15:56:38 +0200

    [Ent Server 10.1.4 QP] EN-89731 Prevent creation of non-existing Enterprise users in Enterprise. Rework on previous commit.
    Former-commit-id: c02fb94b022f27424f0404c77a2bdba872a774e1

--- Enterprise/plugins/release/Elvis/util/ElvisUserUtils.class.php
+++ Enterprise/plugins/release/Elvis/util/ElvisUserUtils.class.php
@@ -3,60 +3,6 @@
 class ElvisUserUtils
 {
 	/**
-	 * Get Enterprise user or when not found, creates a new one.
-	 *
-	 * @deprecated 10.1.4 QP Function is not recommended. A user should not be created on-the-lfy when it is not found.
-	 * @param string $username
-	 * @return AdmUser|null
-	 */
-	public static function getOrCreateUser( $username )
-	{
-		if (empty($username)) {
-			return null;
-		}
-
-		$user = self::getUser($username);
-
-		if (is_null($user)) {
-			require_once BASEDIR . '/server/utils/VersionUtils.class.php';
-			$serverVer = explode( ' ', SERVERVERSION ); // split '9.2.0' from 'build 123'
-			if (VersionUtils::versionCompare( $serverVer[0], '9.4.0', '>=' )) {
-				// The user is not found
-				$user = self::createUser($username);
-			} else {
-				require_once dirname(__FILE__).'/../config.php';
-				$user = self::getUser(ELVIS_ENT_ADMIN_USER);
-			}
-		}
-
-		return $user;
-	}
-
-	/**
-	 * Creates a new Enterprise user.
-	 *
-	 * @deprecated 10.1.4 QP Refer to getOrCreateUser().
-	 * @param string $username
-	 * @return AdmUser|null
-	 */
-	private static function createUser( $username )
-	{
-		require_once BASEDIR . '/server/dbclasses/DBUser.class.php';
-		require_once BASEDIR . '/server/interfaces/services/adm/DataClasses.php';
-		require_once dirname(__FILE__) . '/ElvisUtils.class.php';
-
-		$user = new AdmUser();
-		$user->Name = $username;
-		$user->FullName = $username;
-		$user->ImportOnLogon = true;
-
-		$user = ElvisUtils::enrichUser($user);
-		$user = DBUser::createUserObj($user);
-
-		return $user;
-	}
-
-	/**
 	 * Get the Enterprise user from the database.
 	 *
 	 * @param string $username
@@ -66,7 +12,7 @@ class ElvisUserUtils
 	{
 		require_once BASEDIR.'/server/dbclasses/DBUser.class.php';
 
-		$userRow = DBUser::findUser( null, $username, null );
+		$userRow = DBUser::findUser( null, $username, $username );
 		//LogHandler::logPhpObject($userRow, 'var_dump', 'User Row ' . $username);
 
 		return is_null( $userRow ) ? null : self::rowToUserObj( $userRow );
