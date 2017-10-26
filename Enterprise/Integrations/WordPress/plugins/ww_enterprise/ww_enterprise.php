67db4f924ff5a0e2ec7836e7b8890a590f4d73ad
Jenkins Master <jenkins@woodwing.com>
2017-10-26 14:40:46 +0200

    [Ent Server 10.2.0] Jenkins: Updated product version info files with 204.
    Former-commit-id: 2927f4dbf50a68d28a8ae6611bcfcad21889037f

--- Enterprise/Integrations/WordPress/plugins/ww_enterprise/ww_enterprise.php
+++ Enterprise/Integrations/WordPress/plugins/ww_enterprise/ww_enterprise.php
@@ -3,7 +3,7 @@
  * Plugin Name: WoodWing Enterprise
  * Plugin URI: http://woodwing.com
  * Description: This plugin extends some functionality from WordPress and is used by Enterprise Server.
- * Version: 10.2.0 Build 202
+ * Version: 10.2.0 Build 204
  * Author: WoodWing Software
  */
 
