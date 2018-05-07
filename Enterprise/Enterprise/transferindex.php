927d1963f0f19543980a7c7e794ca015da69307e
Edwin van der Klaauw <edwin.van.der.klaauw@EklBookPro.local>
2018-05-07 16:58:19 +0200

    [Ent Server 10.5.0] [PD-28] Let the Transfer Server rely on the ticket expiration optimization of DBTicket that has been improved. See DBTicket::checkTicket() that calls DBTicket::doesTicketNeedToExtend(). So no longer need to use its own TicketValidationStamp option in the PHP session data.
    Former-commit-id: 2f66580012c19fcf9e89b227f7332338fb9fd405

--- Enterprise/Enterprise/transferindex.php
+++ Enterprise/Enterprise/transferindex.php
@@ -125,33 +125,16 @@ class TransferEntry
 			header('Status: 400 Bad Request - '.$message );
 			LogHandler::Log( 'TransferServer', 'ERROR', $message );
 			exit( $message );
-		} else {
-			BizSession::setTicketCookieForClientIdentifier($ticket);
 		}
-		
-		// The ticket validation takes around 50ms because of the db UPDATE statement.
-		// This is very expensive in context of Publication Overview whereby hundreds
-		// of thumbs are downloaded. Therefore the ticket validation time is saved in the 
-		// session data. When the last saved time is less than one minute ago, the ticket
-		// validation through db is skipped.
-		$validateTicket = true;
+
+		BizSession::setTicketCookieForClientIdentifier( $ticket );
 		session_id( $ticket );
-		$sessionVars = self::getSessionVariables();
-		if( isset($sessionVars['TicketValidationStamp']) ) {
-			$lastSec = intval($sessionVars['TicketValidationStamp']);
-			$nowSec = time(); // the number of seconds since the Unix Epoch (1970)
-			if( $nowSec - $lastSec < 60 ) { // last ticket update was less than one minute ago?
-				$validateTicket = false;
-			}
-		}
 
 		// The OPTIONS call is send by a web browser as a pre-flight for a CORS request.
 		// This request doesn't send or receive any information. There is no need to validate the ticket,
 		// and when the OPTIONS calls returns an error the error can't be validated within an application.
 		// This is a restriction by web browsers.
-		if ( $httpMethod == 'OPTIONS' ) {
-			$validateTicket = false;
-		}
+		$validateTicket = $httpMethod !== 'OPTIONS';
 		
 		// Validate ticket. This implicitly updates ticket expiration date, which is wanted
 		// since clients might do a long upload of many files and then call CreateObjects.
@@ -173,11 +156,6 @@ class TransferEntry
 				exit( $message );
 			}
 			unset( $user );
-			
-			// Remember when we already have validated and updated the ticket expiration.
-			$sessionVars = array();
-			$sessionVars['TicketValidationStamp'] = time();
-			self::setSessionVariables( $sessionVars );
 		}
 				
 		// Anti hijack check (e.g. block people messing around on the file system with .. or *)
@@ -710,46 +688,4 @@ class TransferEntry
 			LogHandler::Log( 'TransferServer', 'DEBUG', $message );
 		}
 	}*/
-
-	/**
-	 * Set multiple session variables.
-	 * This function open the session, sets variables, saves and closes the
-	 * session. This way an other process won't be locked.
-	 *
-	 * Please note: with concurrent processes the last call to
-	 * setSessionVariables will overrule the earlier one
-	 *
-	 * @param array $variables key values pairs
-	 */
-	private static function setSessionVariables( array $variables )
-	{
-		session_start();
-		if( !isset( $_SESSION[self::SESSION_NAMESPACE] ) ) {
-			$_SESSION[self::SESSION_NAMESPACE] = array();
-		}
-		foreach( $variables as $key => $value ) {
-			$_SESSION[self::SESSION_NAMESPACE][$key] = $value;
-		}
-		session_write_close();
-	}
-
-	/**
-	 * Get mulitple session variables.
-	 * This function open the session, gets variables and closes the
-	 * session. This way an other process won't be locked.
-	 * 
-	 * @return array key value pairs
-	 */
-	private static function getSessionVariables()
-	{
-		$variables = array();
-		session_start();
-		if( isset( $_SESSION[self::SESSION_NAMESPACE] ) ) {
-			foreach( $_SESSION[self::SESSION_NAMESPACE] as $key => $value ) {
-				$variables[$key] = $value;
-			}
-		}
-		session_write_close();
-		return $variables;
-	}
 }
