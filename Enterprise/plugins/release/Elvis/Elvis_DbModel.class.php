38b99899f86fba2e6ce33a1dc971b0a10c1860ab
Maarten Groot <mgr@woodwing.com>
2018-05-17 13:38:15 +0200

    [Ent Server 10.5.0] [PD-11] Save and retrieve access token in DB.
    Former-commit-id: 3903a275d71677497ba04e22e378871633c2faec

---
+++ Enterprise/plugins/release/Elvis/Elvis_DbModel.class.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package    Elvis
+ * @subpackage ServerPlugins
+ * @since      10.5
+ * @copyright  WoodWing Software bv. All Rights Reserved.
+ */
+
+require_once BASEDIR.'/server/interfaces/plugins/connectors/DbModel_EnterpriseConnector.class.php';
+
+require_once dirname(__FILE__).'/dbmodel/Definition.class.php';
+
+class Elvis_DbModel extends DbModel_EnterpriseConnector
+{
+	/**
+	 * @inheritdoc
+	 */
+	public function getDbModelProvider()
+	{
+		return new Elvis_DbModel_Definition();
+	}
+
+}
---
+++ Enterprise/plugins/release/Elvis/dbclasses/Token.class.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package    Elvis
+ * @subpackage DbClasses
+ * @since      10.5
+ * @copyright  WoodWing Software bv. All Rights Reserved.
+ */
+
+require_once BASEDIR.'/server/dbclasses/DBBase.class.php';
+
+class Elvis_DbClasses_Token extends DBBase
+{
+	const TABLENAME = 'lvs_tokens';
+
+	public static function save( $user, $accessToken )
+	{
+		$values = array( 'user' => $user, 'token' => $accessToken );
+		//	rather update before insert because it's more likely it exists already but there's no way to see if the update
+		// updated a row or not
+		$insertResult = self::insertRow( self::TABLENAME, $values );
+		if( $insertResult === false ) {
+			// record already exists, update it. Ignore if it fails?
+			self::updateRow( self::TABLENAME, $values, '`user` = ?', array( $user ) );
+		}
+	}
+
+	public static function get( $user )
+	{
+		LogHandler::Log( 'ELVIS', 'DEBUG', 'user: '.print_r( $user, true ) );
+		$row = self::getRow( self::TABLENAME, '`user` = ?', array( 'token' ), array( $user ) );
+		LogHandler::Log( 'ELVIS', 'DEBUG', 'row: '.print_r( $row, true ) );
+		return array_key_exists( 'token', $row ) ? $row['token'] : null;
+	}
+}
