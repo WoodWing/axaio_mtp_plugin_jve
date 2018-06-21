eb76901c6ab49b54fc2793b61d00d2fdb7169aa2
Edwin van der Klaauw <>
2018-06-21 17:13:17 +0200

    [Ent Server 10.5.0] [PD-28] Rework on session optimisation; Show menu structure (in left pane) when access rights error dialog is shown for brand admin who tries to access system admin page.
    Former-commit-id: 1c9af7bca7f0334cf292358b04a498aadf7fcbfc

--- Enterprise/Enterprise/server/apps/noright.php
+++ Enterprise/Enterprise/server/apps/noright.php
@@ -1,6 +1,10 @@
 <?php
 require_once __DIR__.'/../../config/config.php';
+require_once BASEDIR.'/server/admin/global_inc.php';
+require_once BASEDIR.'/server/secure.php';
 require_once BASEDIR.'/server/utils/htmlclasses/HtmlDocument.class.php';
+
+checkSecure( 'publadmin' );
 
 $tpl = HtmlDocument::loadTemplate( 'noright.htm' );
 print HtmlDocument::buildDocument( $tpl );
